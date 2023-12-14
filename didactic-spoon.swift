2694cdba958f60326e902178d0d6546d2a82530b
Barq <152813899+vulkanovmihail@users.noreply.github.com>
2023-12-14 14:20:24 -0600

    Create didactic-spoon.swift

---
+++ didactic-spoon.swift
@@ -0,0 +1,12 @@
+        newState.selectedPointsIDs.update(with: pointID)
+        
+        return newState
+    }
+            newCollage.dependencies.append(.init(pointIDs: ids))
+            return newCollage
+        }
+        
+        newCollage.dependencies[index] = .init(pointIDs: ids)
+        
+        return newCollage
+    }
